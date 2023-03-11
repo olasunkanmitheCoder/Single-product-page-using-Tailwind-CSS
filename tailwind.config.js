176afa2b149f224edbe33ea890905defbe68769e
olasunkanmitheCoder <olasunkanmidaniel003@gmail.com>
2023-03-11 01:14:01 +0100

    I will get a job soon, lol

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./build/*html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        productgreen: "#788570"
+      }
+    },
+  },
+  plugins: [],
+}
